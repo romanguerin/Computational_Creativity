feaea1a5b8010b00a7e554852c82f5da42f33ce4
Roman Guerin <romanguerin@msn.com>
2021-12-04 16:47:55 +0100

    weekend update

--- sketch.js
+++ sketch.js
@@ -2,12 +2,19 @@
 // https://inspirit.github.io/jsfeat/sample_canny_edge.html
 
 let capture;
+let img;
 let buffer;
 let result;
-let w = 640,
+let w = 480,
     h = 480;
+let coordinates = [];
+
+function preload() {
+    img = loadImage('assets/3d_plot.PNG');
+}
 
 function setup() {
+    //arr.push(coordinates);
     frameRate(10);
     capture = createCapture({
         audio: false,
@@ -23,6 +30,7 @@ function setup() {
     capture.size(w, h);
     capture.hide();
     buffer = new jsfeat.matrix_t(w, h, jsfeat.U8C1_t);
+
 }
 
 function jsfeatToP5(src, dst) {
@@ -45,37 +53,35 @@ function jsfeatToP5(src, dst) {
 }
 
 function raster(rastNumb) {
-    let numb = rastNumb;
-    let numbX = numb * 1.33333333333;
+    let numb = floor(rastNumb/10)*10;
+    if (numb === 50){
+        numb = 60;
+    }
+    let numbX = numb;
     let numbY = numb;
-    push();
     stroke('rgba(0,255,0,0.25)');
     strokeWeight(2);
-    let xN = floor(width/numbX);
-    let yN = floor(height/numbY);
+    let xN = floor(w/numbX);
+    let yN = floor(h/numbY);
     //detect colors
     colorDetection(numbX,numbY,xN,yN);
-
     for (let i = 1; i < numbX; i++){
-        line(xN*i, 0, xN*i, height);
-        /*for (let m = 1; m < numbY; m++){
-            ellipse(xN*i,yN*m,100/numb);
-        }*/
+        line(xN*i, 0, xN*i, h);
     }
     for (let j = 1; j < numbY; j++){
-        line(0, yN*j, width, yN*j);
+        line(0, yN*j, w, yN*j);
     }
-    pop();
 }
 
 function colorDetection(numbX,numbY,xN,yN) {
+    let arrInd = 0;
     let index = 0;
     let rgba = [];
     let black = color('rgba(0,0,0,1)');
     let c = color(255, 0, 0);
     loadPixels();
     let xPixel = (4 * xN * 2);
-    let yPixel = ((yN*2)*(4 * width)*2) ;
+    let yPixel = ((yN*2)*(4 * w)*2) ;
     for (let i = 1; i < numbY; i++) {
         for (let j = 1; j < numbX; j++) {
             // loop over
@@ -85,11 +91,17 @@ function colorDetection(numbX,numbY,xN,yN) {
             rgba[0] = pixels[index];
             rgba[1] = pixels[index + 1];
             rgba[2] = pixels[index + 2];
-            rgba[3]= pixels[index + 3];
+            rgba[3] = pixels[index + 3];
             //console.log('pixel:',x,y,"color",r,g,b,a);
             if (!arraysEqual(black.levels,rgba)){
                 fill(c);
-                ellipse(x, y, 8);
+                ellipse(x, y, 6);
+                //push array
+                if (arrInd === 0) {
+                    coordinates = [];
+                }
+                coordinates.push({'i': arrInd++,'x': x, 'y': y })
+                arrInd++;
             } else {
                 fill(0);
             }
@@ -103,9 +115,13 @@ function arraysEqual(a1,a2) {
 }
 
 function draw() {
-    image(capture, 0, 0, 640, 480);
+    //console.log(coordinates.length,coordinates[floor(coordinates.length/2)]);
+    image(capture, 0, 0, 480, 480);
     capture.loadPixels();
     if (capture.pixels.length > 0) { // don't forget this!
+        result = jsfeatToP5(buffer, result);
+        image(result, 0, 0, 480, 480);
+
         let blurSize = select('#blurSize').elt.value;
         let lowThreshold = select('#lowThreshold').elt.value;
         let highThreshold = select('#highThreshold').elt.value;
@@ -115,31 +131,15 @@ function draw() {
         lowThreshold = map(lowThreshold, 0, 100, 0, 255);
         highThreshold = map(highThreshold, 0, 100, 0, 255);
         rast = map(rast, 0, 100, 10, 60);
-
         jsfeat.imgproc.grayscale(capture.pixels, w, h, buffer);
         jsfeat.imgproc.gaussian_blur(buffer, buffer, blurSize, 0);
         jsfeat.imgproc.canny(buffer, buffer, lowThreshold, highThreshold);
-        let n = buffer.rows * buffer.cols;
+        //let n = buffer.rows * buffer.cols;
         // uncomment the following lines to invert the image
        // for (var i = 0; i < n; i++) {
        //     buffer.data[i] = 255 - buffer.data[i];
        // }
-        result = jsfeatToP5(buffer, result);
-        image(result, 0, 0, 640, 480);
+        //console.log(arr);
         raster(rast);
     }
 }
-
-
-/*
-function colorDetection(numbX,numbY){
-    let c = get(50, 90);
-    let black = color('rgba(0,0,0,1)');
-    if(!arraysEqual(c,black.levels)){
-        console.log("color",c);
-        console.log("black",black.levels);
-    }
-    fill(c);
-    noStroke();
-    rect(25, 25, 50, 50);
-}*/
